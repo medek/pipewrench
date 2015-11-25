71c7ce0f04dfcbb63917c732dd8f23840be3178a
Gavin Massey <mdk@mystacktrace.org>
2015-11-25 23:45:50 +0000

    Circle/Line intersection
    also Circle/Point intersection since if someone is stupid and passes
    in a zero length line, it needs to be accounted for; otherwise it
    throws off the code into thinking it's a tangent when it isn't

--- src/collision/primitive.rs
+++ src/collision/primitive.rs
@@ -1,5 +1,5 @@
 use Thingie as Base;
-use nalgebra::{Pnt2, Vec2, Cross, Dot};
+use nalgebra::{Pnt2, Vec2, Cross, Dot, POrd};
 use super::{Intersect, Intersection};
 
 pub struct Circle<S: Base> {
@@ -18,6 +18,15 @@ impl<S> Line<S> where S: Base {
         Line {
             a: a,
             b: b
+        }
+    }
+}
+
+impl<S> Circle<S> where S: Base {
+    pub fn new(pos: Pnt2<S>, radius: S) -> Circle<S> {
+        Circle {
+            pos: pos,
+            radius: radius
         }
     }
 }
@@ -69,30 +78,109 @@ impl<S> Intersect<Line<S>, S> for Line<S> where S: Base {
     }
 
     fn contains(&self, other: &Line<S>) -> bool {
-        false
+        //this function should probably just return false
+        match self.intersection(other) {
+            Intersection::Overlap(a, b) => {
+                a.x >= self.a.x && a.y >= self.a.y && b.x <= self.b.x && b.y <= self.b.y
+            },
+            _ => false
+        }
     }
 }
 
-#[test]
-fn line_intersects() {
-    // no intersect
-    assert_eq!(Line::new(Pnt2::new(0.0,0.0), Pnt2::new(2.0, 8.0))
-               .intersects(&Line::new(Pnt2::new(8.0, 0.0), Pnt2::new(0.0, 20.0))), false);
-    // intersect
-    assert_eq!(Line::new(Pnt2::new(0.0,10.0), Pnt2::new(2.0, 0.0))
-               .intersects(&Line::new(Pnt2::new(10.0, 0.0), Pnt2::new(0.0, 5.0))), true);
-    // parallel, vertical
-    assert_eq!(Line::new(Pnt2::new(0.0,0.0), Pnt2::new(0.0, 10.0))
-               .intersects(&Line::new(Pnt2::new(2.0, 0.0), Pnt2::new(2.0, 10.0))), false);
-    // parallel, diagonal
-    assert_eq!(Line::new(Pnt2::new(0.0,0.0), Pnt2::new(5.0, 5.0))
-               .intersects(&Line::new(Pnt2::new(2.0, 0.0), Pnt2::new(7.0, 5.0))), false);
-    //collinear, overlap
-    assert_eq!(Line::new(Pnt2::new(0.0,0.0), Pnt2::new(5.0, 5.0))
-               .intersects(&Line::new(Pnt2::new(2.0, 2.0), Pnt2::new(7.0, 7.0))), true);
-    //collinear, no overlap
-    assert_eq!(Line::new(Pnt2::new(0.0,0.0), Pnt2::new(5.0, 5.0))
-               .intersects(&Line::new(Pnt2::new(7.0, 7.0), Pnt2::new(10.0, 10.0))), false);
+impl<S> Intersect<Line<S>, S> for Circle<S> where S: Base {
+    fn intersection(&self, other: &Line<S>) -> Intersection<S> {
+        let two = S::from(2.0).unwrap();
+        let d = (other.b - other.a);
+
+        if d.x == S::zero() && d.y == S::zero() { //line has zero length
+            //I could be an asshole and let it pretend it's a tangent and return NaNs,
+            //I guess I'm just that nice
+            return self.intersection(&other.a)
+        }
+
+        let f = (other.a - self.pos);
+        let a = d.dot(&d);
+        let b = S::from(2.0).unwrap()*f.dot(&d);
+        let c = f.dot(&f) - self.radius.powf(two);
+
+        let mut dis = b.powf(two) - (S::from(4.0).unwrap() * a * c);
+        if dis < S::zero() {
+            return Intersection::Outside
+        }
+        else if dis == S::zero() {
+            dis = dis.sqrt();
+
+            let t0 = (-b - dis)/(two * a);
+            let t1 = (-b + dis)/(two * a);
+            println!("zero\t{}, {}", t0, t1);
+            return Intersection::Intersects(other.a + (d * t0), None)
+        }
+        else {
+            dis = dis.sqrt();
+
+            let t0 = (-b - dis)/(two * a);
+            let t1 = (-b + dis)/(two * a);
+
+            println!(">zero\t{}, {}", t0, t1);
+            if t0 < S::zero() && t1 <= S::one() { //A inside circle
+                return Intersection::Intersects(other.a + (d * t1), None)
+            }
+            else if t1 > S::one() && t0 >= S::zero() { //B inside circle
+                return Intersection::Intersects(other.a + (d * t0), None)
+            }
+            else if t1 > S::one() && t0 < S::zero() { //all inside
+                return Intersection::Inside
+            }
+            else if t0 > S::zero() && t0 < S::one() && t1 > S::zero() && t1 < S::one() { //two intersections
+                return Intersection::Intersects(other.a + (d * t0), Some(other.a + (d * t1)))
+            }
+        }
+        Intersection::Outside
+    }
+
+    fn intersects(&self, other: &Line<S>) -> bool {
+        match self.intersection(other) {
+            Intersection::Intersects(_, _) => true,
+            _ => false
+        }
+    }
+
+    fn contains(&self, other: &Line<S>) -> bool {
+        match self.intersection(other) {
+            Intersection::Inside => true,
+            _ => false
+        }
+    }
+}
+
+impl<S> Intersect<Pnt2<S>, S> for Circle<S> where S: Base {
+    fn intersection(&self, other: &Pnt2<S>) -> Intersection<S> {
+        let d = *other - self.pos;
+
+        if d.x.abs() < self.radius && d.y.abs() < self.radius {
+            return Intersection::Inside
+        }
+        else if d.x.abs() > self.radius && d.y.abs() > self.radius {
+            return Intersection::Outside
+        }
+        else {
+            return Intersection::Intersects(other.clone(), None)
+        }
+    }
+
+    fn intersects(&self, other: &Pnt2<S>) -> bool {
+        let d = *other - self.pos;
+        //should probably add an epsilon-y type check here but eh
+        d.x.abs() == self.radius && d.y.abs() == self.radius
+    }
+
+    fn contains(&self, other: &Pnt2<S>) -> bool {
+        match self.intersection(other) {
+            Intersection::Inside => true,
+            _ => false
+        }
+    }
 }
 
 #[test]
@@ -134,5 +222,62 @@ fn line_intersection() {
     l1.b.y = 15.0;
     //it is right but there's stupid floating point mess
     assert_eq!(l1.intersection(&l2), Intersection::Overlap(Pnt2 {x: 7.0, y: 7.0 }, Pnt2 {x: 10.000000000000002, y: 10.000000000000002}));
+    assert_eq!(l1.contains(&l2), true)
+}
+
+#[test]
+fn circle_line_intersection() {
+    let circle = Circle::new(Pnt2::new(3.0,1.0), 6.0);
+    let mut line = Line::new(Pnt2::new(-3.0, -3.0), Pnt2::new(-3.0, 3.0));
+
+    //tangent
+    assert_eq!(circle.intersection(&line), Intersection::Intersects(Pnt2 {x: -3.0, y: 1.0}, None));
+
+    line.a = Pnt2::new(-6.0, 1.0);
+    line.b = Pnt2::new(12.0, 1.0);
+
+    //intersection A -> B
+    assert_eq!(circle.intersection(&line), Intersection::Intersects(Pnt2 {x: -3.0, y: 1.0}, Some(Pnt2 {x: 9.0, y: 1.0})));
+
+    let temp = line.a;
+    line.a = line.b;
+    line.b = temp;
+
+    //intersecion B -> A
+    assert_eq!(circle.intersection(&line), Intersection::Intersects(Pnt2 {x: 9.0, y: 1.0}, Some(Pnt2 {x: -3.0, y: 1.0})));
+
+    line.b = line.a;
+    line.a = circle.pos.clone();
+    //intersection A inside circle
+    assert_eq!(circle.intersection(&line), Intersection::Intersects(Pnt2 {x: 9.0, y: 1.0}, None));
+
+    line.a = Pnt2::new(-6.0, 1.0);
+    line.b = circle.pos;
+
+    //intersection B inside circle
+    assert_eq!(circle.intersection(&line), Intersection::Intersects(Pnt2 {x: -3.0, y: 1.0}, None));
+
+
+    line.a = circle.pos + (circle.radius/2.0);
+    line.b = circle.pos;
+
+    //line completely inside
+    assert_eq!(circle.intersection(&line), Intersection::Inside);
+
+    line.a = Pnt2::new(-20.0, 20.0);
+    line.b = Pnt2::new(20.0, 20.0);
+
+    //line completely outside
+    assert_eq!(circle.intersection(&line), Intersection::Outside);
+
+    line.b = line.a.clone();
+    //zero length line outside
+    assert_eq!(circle.intersection(&line), Intersection::Outside);
+
+    line.a = circle.pos.clone();
+    line.b = line.a.clone();
+
+    //zero length line inside
+    assert_eq!(circle.intersection(&line), Intersection::Inside);
 }
 
